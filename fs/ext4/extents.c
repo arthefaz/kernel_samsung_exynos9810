26e762aef24e99f46272f02adca8faed1912f695
Dan Carpenter <dan.carpenter@oracle.com>
2016-12-03 16:46:58 -0500

    ext4: remove another test in ext4_alloc_file_blocks()
    Before commit c3fe493ccdb1 ('ext4: remove unneeded test in
    ext4_alloc_file_blocks()') then it was possible for "depth" to be -1
    but now, it's not possible that it is negative.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -4727,7 +4727,7 @@ retry:
 		/*
 		 * Recalculate credits when extent tree depth changes.
 		 */
-		if (depth >= 0 && depth != ext_depth(inode)) {
+		if (depth != ext_depth(inode)) {
 			credits = ext4_chunk_trans_blocks(inode, len);
 			depth = ext_depth(inode);
 		}
